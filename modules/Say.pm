8f0243cac28414f8171d73fe57d20eba40f30bd6
Patrick Burroughs (Celti) <celticmadman@gmail.com>
2015-05-27 12:08:20 -0700

    Added support for changing channel/user modes.

--- modules/Say.pm
+++ modules/Say.pm
@@ -3,7 +3,7 @@ package Bot::BasicBot::Pluggable::Module::Say;
 use base qw(Bot::BasicBot::Pluggable::Module);
 use common::sense;
 
-our $VERSION = '1';
+our $VERSION = '3';
 
 sub help {
 	return "Speaks with the voice of the bot. Requires auth. Usage: !say <expression>, !do <action>";
@@ -15,7 +15,7 @@ sub told {
 	return unless defined($body);
 
 	return unless $self->authed($message->{who});
-	return unless $body =~ /^(?:!|\.)(say|do)\s+(\#\S+)\s+(.+)$/i;
+	return unless $body =~ /^(?:!|\.)(say|do|mode)\s+(\#\S+)\s+(.+)$/i;
 
 	my $type = $1;
 	my $chan = $2;
@@ -23,6 +23,7 @@ sub told {
 
 	return $self->bot->say({channel => $chan, body => $mess}) if $type eq 'say';
 	return $self->bot->emote({channel => $chan, body => $mess}) if $type eq 'do';
+	return $self->bot->mode("$chan $mess") if $type eq 'mode';
 }
 
 1;
